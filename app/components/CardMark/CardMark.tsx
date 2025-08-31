fdc904fae1943c7c9041cc442f6c37819f93ab63
Andrew Kruhluk <kruglikandrej9@gmail.com>
2025-08-31 23:30:18 +0300

    hw

--- app/components/CardMark/CardMark.tsx
+++ app/components/CardMark/CardMark.tsx
@@ -7,6 +7,7 @@ import { Navigation, Thumbs} from 'swiper/modules';
 import { SwiperSlide, Swiper as Slide } from "swiper/react";
 
 
+
 interface CardMarkProps{
     id: string
 }
@@ -18,7 +19,7 @@ interface ItemProps{
     sizes: string[]
 }
 const items = [
-    {id: '1', imgFront: '/items/one-rb.png', price: '3499 UAH', name: 'Project G/R CUNTIER ZIP HOODIE', sizes: ['S', 'M', 'L']},
+    {id: '1', imgFront: '/items/one.jpg', price: '3499 UAH', name: 'Project G/R CUNTIER ZIP HOODIE', sizes: ['S', 'M', 'L']},
     {id: '2', imgFront: '/items/two.jpg', price: '3099 UAH', name: 'Project G/R LONDON ZIP HOODIE', sizes: ['S', 'M', 'L']},
     {id: '3', imgFront: '/items/three.jpg', price: '3499 UAH', name: 'Project G/R CUNTIER ZIP HOODIE', sizes: ['S', 'M', 'L']},
     {id: '4', imgFront: '/items/four.jpg', price: '3499 UAH', name: 'Project G/R CUNTIER ZIP HOODIE', sizes: ['S', 'M', 'L']}
@@ -44,46 +45,25 @@ function CardMark({id}: CardMarkProps){
             <div className={css.container}>
                 <div className={css.blockRes}>
                     <div className={css.gallery}>
-                    
-                        <div className={css.thumbsWrapper}>
-                            <Slide
-                                direction='vertical'
-                                onSwiper={setThumbsSwiper}
-                                spaceBetween={10}
-                                slidesPerView={2}
-                                watchSlidesProgress
-                                modules={[Thumbs]}
-                                className={css.thumbs}
-                            >
-                                
-                            
-                                
-                                <SwiperSlide >
-                                    {item !== undefined &&
-                                        <Image src={item.imgFront} alt={item.name} width={80} height={80} />
-                                    }
-                                    
-                                </SwiperSlide>
-                                <SwiperSlide >
-                                    {item !== undefined &&
-                                        <Image src={item.imgFront} alt={item.name} width={80} height={80} />
-                                    }
-                                    
-                                </SwiperSlide>
-                                
-                            </Slide>
-                        </div>
+                        
+                        
                     </div>    
 
                     <div>
                         <div className="swiper">
                             
                            <Slide
+                                
                                 spaceBetween={10}
-                                navigation={true}
+                                navigation={{
+                                        nextEl: '.swiper-button-next',
+                                        prevEl: '.swiper-button-prev',
+                                }}
                                 thumbs={{ swiper: thumbsSwiper }}
                                 modules={[Navigation, Thumbs]}
                                 className={css.mainSwiper}
+                                
+                                
 
                             >
                                 
@@ -93,16 +73,18 @@ function CardMark({id}: CardMarkProps){
                                     {item !== undefined &&
                                         <Image src={item.imgFront} alt={item.name} width={500} height={500} />
                                     }
-                                    
+                                    ...
                                 </SwiperSlide>
                                 <SwiperSlide >
                                     {item !== undefined &&
                                         <Image src={item.imgFront} alt={item.name} width={500} height={500} />
                                     }
-                                    
+                                    ...
                                 </SwiperSlide>
                                 
                             </Slide>
+
+                            
                             
 
                             
